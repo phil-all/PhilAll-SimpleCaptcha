89c337ec3e7c4f47c4eb6f88f971fa9f1f932a3b
phil-all <allard.latour.philippe@gmail.com>
2021-12-07 09:34:58 +0100

    :sparkles:  Generate a token to be store in a cookie #7

---
+++ src/PhillAll/SimpleCaptcha/Token.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * This file is part of the PhilAll.SimpleCaptcha.
+ *
+ * PHP version 7.4
+ *
+ * @category PhillAll
+ * @package  SimpleCaptcha
+ * @author   Philippe Allard-Latour <phil.all.dev@gmail.com>
+ * @license  https://opensource.org/licenses/MIT MIT
+ * @link     https://github.com/phil-all/SimpleCaptcha
+ * @since    File available since Release 0.9 Beta
+ *
+ * This package is Open Source.
+ */
+
+declare(strict_types=1);
+
+namespace PhillAll\SimpleCaptcha;
+
+use PhillAll\SimpleCaptcha\Cookie;
+
+/**
+ * A light token manager for SCWT (SimpleCaptcha web token)
+ *
+ * @category PhillAll
+ * @package  SimpleCaptcha
+ * @author   Philippe Allard-Latour <phil.all.dev@gmail.com>
+ * @license  https://opensource.org/licenses/MIT MIT
+ * @link     https://github.com/phil-all/SimpleCaptcha
+ * @since    File available since Release 0.9 Beta
+ */
+Class Token
+{
+    /**
+     * string to use to generate token
+     *
+     * @var string $string
+     */
+    private $string;
+    /**
+     * Sets string to use to generate token
+     *
+     *@param string $string string to be encrypt to generate token
+     */
+    public function __construct($string)
+    {
+        $this->string = $string;
+
+        return $this;
+    }
+
+    /**
+     * Encrypts a given string.
+     *
+     * @return string
+     */
+    public function encrypt(): string
+    {
+        return password_hash(strrev($this->string), PASSWORD_DEFAULT);
+    }
+
+    /**
+     * Stores token in a cookie.
+     *
+     * @param string $string SimpleCaptcha
+     *
+     * @return void
+     */
+    public function store(): void
+    {
+        $store = (new Cookie)->setCOOKIE('SCWT', $this->encrypt());
+    }
+
+    /**
+     * Checks if a given string matches string use to generate SCWT token.
+     *
+     * @param string $given
+     * @param string $cookie
+     *
+     * @return boolean
+     */
+    public function compare(string $given, string $cookie): bool
+    {
+        return password_verify(strrev($given), (new Cookie)->getCOOKIE('SCWT'));
+    }
+}
